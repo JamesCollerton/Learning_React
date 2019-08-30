fc0835dbee5bfdd4113d42d8013592a2118b92fe
James Collerton <jamescollerton@live.co.uk>
2019-08-30 14:49:19 +0100

    Added default params etc

--- 2-emerging-javascript/emerging-javascript.js
+++ 2-emerging-javascript/emerging-javascript.js
@@ -1,5 +1,30 @@
+const contantString = "Constant"
+
 function demo() {
-    console.log("Hello")
+    constantFunction()
+    lexicalScopingAndTemplateStrings()
+    defaultParameters()
+}
+
+function defaultParameters(parameter = "Parameter") {
+    console.log(`Default parameter: ${parameter}`)
+}
+
+function lexicalScopingAndTemplateStrings() {    
+    var outer = 0;
+    var inner = 0;
+
+    for(var i = 0; i < 3; i++) {
+        var outer = i
+        let inner = i
+    }
+
+    console.log(`Outer: ${outer}`)
+    console.log(`Inner: ${inner}`)
+}
+
+function constantFunction() {
+    console.log(contantString)
 }
 
 demo()
